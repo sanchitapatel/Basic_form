8d5e0fd47e8d094f767bc6b20791b8c7a4a97d2e
sanchitapatel <sanchita15pa@gmail.com>
2024-08-22 18:05:59 -0700

    first commit

--- app/views.py
+++ app/views.py
@@ -1,3 +1,21 @@
+
 from django.shortcuts import render
+from .forms import *
 
 # Create your views here.
+
+
+def home(request):
+    form=Registration()
+    # return render(request,"home.html",{"form":form})
+    if request.method=="POST":
+        form=Registration(request.POST)
+        
+        if form.is_valid():
+            fname=form.cleaned_data["fname"]
+            lname=form.cleaned_data["lname"]
+            email=form.cleaned_data["email"]
+            contact=form.cleaned_data["contact"]
+            print(fname,lname,email,contact)
+            data={"fname":fname,"lname":lname,"email":email,"contact":contact}
+    return render(request,"home.html",{"form":form})
